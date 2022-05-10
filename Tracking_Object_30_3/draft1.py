362f64f3c09b03cba6f73d8f15456c8a689b2665
Truong Minh Hoang <hoangminhkiu@gmail.com>
2022-05-10 19:52:18 +0700

    Delete Tracking_Object_30_3 directory

--- Tracking_Object_30_3/draft1.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-
-
-def get_mask_green(hsv_img):
-    mask1 = cv2.inRange(hsv_img, np.array([37, 139, 126]), np.array([83, 255, 227]))
-    mask = mask1
-    return mask
