ee46232cc8e17e7ff6eee740c66a900fba4f68d8
Truong Minh Hoang <hoangminhkiu@gmail.com>
2022-05-10 21:50:06 +0700

    Update mylib.py

--- Python/mylib.py
+++ Python/mylib.py
@@ -5,7 +5,7 @@ import math
 import urllib.request
 import threading
 
-root_url = "//paste url here"
+root_url = "paste url here"
 
 
 # ESP's url, ex: http://192.168.102 (Esp prints it to serial console when connected to wi-fi)
